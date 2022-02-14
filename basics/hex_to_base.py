b17d187fa2134f405847b36e91cbf4619d97032f
Angelo 'Flecart' Huang <huangelo02@gmail.com>
2022-02-14 21:54:50 +0100

    reset folder for rust

--- basics/hex_to_base.py
+++
@@ -1,6 +0,0 @@
-from base64 import b64encode
-string = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
-
-byt = bytes.fromhex(string)
-print(byt)
-print(b64encode(byt));
