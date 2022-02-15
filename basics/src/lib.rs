de497aaf11ccee51b9a5895fa71d1d5b933234d3
Angelo Huang <huangelo02@gmail.com>
2022-02-15 11:08:12 +0100

    solve xor bytes
    learned about rust: Result, option, boxes, a little bit about borrow, about match, about enums, and checked some crates

--- basics/src/lib.rs
+++ basics/src/lib.rs
@@ -1,17 +1,40 @@
 extern crate base64;
+extern crate hex;
 use std::u8;
 
-pub fn hex_to_base64(hex: String) -> String {
-    // from https://stackoverflow.com/questions/26185485/how-to-convert-hexadecimal-values-to-base64-in-rust
-    // Make vector of bytes from octets
-    let mut bytes = Vec::new();
-    for i in 0..(hex.len()/2) {
-        let res = u8::from_str_radix(&hex[2*i .. 2*i+2], 16);
-        match res {
-            Ok(v) => bytes.push(v),
-            Err(e) => println!("Problem with hex: {}", e),
-        };
+pub fn hex_to_base64(hex: String) -> Result<String, hex::FromHexError> {
+    let result = hex::decode(&hex);
+    match result {
+        Ok(bytes) => Ok(base64::encode(&bytes)),
+        Err(e) => Err(e)
+    }
+}
+
+pub fn xor(first: &Vec<u8>, second: &Vec<u8>) -> Result<Vec<u8>, &'static str>  {
+    match first.len() == second.len() {
+        false => Err("The length of the input vectors should be the same"),
+        true => {
+            let mut xored = Vec::new();
+            let len = first.len(); 
+            for i in 0..len {
+                xored.push(first[i] ^ second[i]);
+            }
+            Ok(xored)
+        }
+    }
+}
+
+pub fn xor_hex(first: String, second: String) -> Result<String, &'static str>  {
+    let first_bytes =  match hex::decode(&first) {
+        Err(_) => return Err("Error in decoding the first hex"),
+        Ok(bytes) => bytes
+    };
+    
+    let second_bytes = match hex::decode(&second) {
+        Err(_) => return Err("Error in decoding the second hex"),
+        Ok(bytes) => bytes 
     };
 
-    base64::encode(&bytes) // now convert from Vec<u8> to b64-encoded String
-} 
+    let xored = xor(&first_bytes, &second_bytes)?;
+    Ok(hex::encode(xored))
+}
--- basics/src/main.rs
+++ basics/src/main.rs
@@ -1,8 +1,25 @@
 mod lib;
 
 fn main() {
+    test_xor_hex();
+}
+
+#[allow(dead_code)]
+fn test_hex_to_base64() {
     use lib::hex_to_base64;
 
     let input = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
-    println!("{}", hex_to_base64(input.to_string()));
+    println!("{}", hex_to_base64(input.to_string()).unwrap());
+}
+
+#[allow(dead_code)]
+fn test_xor_hex() {
+    let a = "1c0111001f010100061a024b53535009181c";
+    let b = "686974207468652062756c6c277320657965";
+
+    let test = "746865206b696420646f6e277420706c6179";
+
+    let ouput = lib::xor_hex(a.to_string(), b.to_string()).unwrap();
+    println!("{}", ouput);
+    assert!(test.eq(&ouput));
 }
